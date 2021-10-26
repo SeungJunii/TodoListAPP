f8ab3d4274246b2599799831222bf8fce429fbc1
SeungJunii <ssj7410@gmail.com>
2021-10-26 17:59:09 +0900

    State를 이용한 목록추가

--- src/App.js
+++ src/App.js
@@ -1,13 +1,46 @@
-import React from 'react';
+import React, { useState, useRef, useCallback } from 'react';
 import TodoInsert from './components/TodoInsert';
 import TodoTemplate from './components/TodoTemplate';
 import TodoList from './components/TodoList';
 
 const App = () => {
+  const [todos, setTodos] = useState([
+    {
+      id: 1,
+      text: 'step1',
+      checked: true,
+    },
+    {
+      id: 2,
+      text: 'step2',
+      checked: true,
+    },
+    {
+      id: 3,
+      text: 'step3',
+      checked: false,
+    },
+  ]);
+
+  const nextId = useRef(4);
+
+  const onInsert = useCallback(
+    (text) => {
+      const todo = {
+        id: nextId.current,
+        text,
+        checked: false,
+      };
+      setTodos(todos.concat(todo));
+      nextId.current += 1;
+    },
+    [todos],
+  );
+
   return (
     <TodoTemplate>
-      <TodoInsert />
-      <TodoList />
+      <TodoInsert onInsert={onInsert} />
+      <TodoList todos={todos} />
     </TodoTemplate>
   );
 };
